99f4430f552e2a7cb5f560efc30afced4a1171b3
mshindle <mshindle@riotgames.com>
2018-03-26 17:35:01 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['airbnb-base', 'prettier'],
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'no-param-reassign': 'off', // Needed for swapping elements within an array.
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off', // Indicate private methods.
+    'no-unused-vars': ['error', {argsIgnorePattern: '^_$'}], // Ignore variables called _.
+    'prefer-destructuring': ['error', {'object': false}],
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': false,
+  },
+};
